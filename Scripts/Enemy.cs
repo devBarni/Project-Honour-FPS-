64c0288e2669f172f07cc1014ab6791cfd1a05c5
Kamil Baran <hello.barni1@gmail.com>
2024-11-29 21:38:55 +0100

    Core gameplay

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,90 @@
+using Godot;
+using System;
+
+public partial class Enemy : CharacterBody3D
+{
+    // Reference to the 3D animated sprite for death amimation.
+    private AnimatedSprite3D _animatedSprite3D;
+    
+    // Exported variables to allow configuration in the Godot
+    [ExportCategory("Attributes")] [Export(PropertyHint.Range, "0,10, 0.5")]
+    private float _moveSpeed = 2.0f;
+
+    [Export(PropertyHint.Range, "0,10, 0.5")]
+    private float _attackRange = 1.0f;
+    
+    // Reference to the player node
+    private CharacterBody3D _player;
+    
+    private bool _dead = false;
+
+    public override void _Ready()
+    {
+        // Find the player node in the "player" group
+        _player = GetTree().GetFirstNodeInGroup("player") as CharacterBody3D;
+        
+        // Get reference to the death sound and animated sprite.
+        _animatedSprite3D = GetNode<AnimatedSprite3D>("AnimatedSprite3D");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        
+        // if enemy is dead or player is not found, stop processing
+        if (_dead || _player == null) return;
+        
+        // Calculate direction to player, ignoring vertical difference
+        var dir = _player.GlobalPosition - GlobalPosition;
+        dir.Y = 0.0f;
+        dir = dir.Normalized();
+        
+        // Move towards the player at defined speed
+        Velocity = dir * _moveSpeed;
+        MoveAndSlide();
+        
+        
+        // Attempt to kill player if in range
+        AttemptToKillPlayer();
+    }
+
+    private void AttemptToKillPlayer()
+    {
+        // Calculate distance to player.
+        var distToPlayer = GlobalPosition.DistanceTo(_player.GlobalPosition);
+
+        // If player is outside attack range, do nothing
+        if (distToPlayer > _attackRange) return;
+        
+        // If player is outside attack range, do nothing;
+        var eyeLine = Vector3.Up * 1.5f;
+        
+        // Create ray query parameters to check line of sight.
+        var query = PhysicsRayQueryParameters3D.Create(
+            GlobalPosition + eyeLine,
+            _player.GlobalPosition + eyeLine,
+            1);
+        
+        // Perform ray cast to check for obstacles
+        var resultObstacles = GetWorld3D().DirectSpaceState.IntersectRay(query);
+
+        if (resultObstacles.Count == 0)
+        {
+            _player.Call("Kill");
+        }
+    }
+    
+    // Method to handle enemy's death
+    private void Kill()
+    {
+        _dead = true;
+        
+        GetNode<AudioStreamPlayer3D>("DeathSound").Play();
+        
+        // Play death animation.
+        _animatedSprite3D.Play("death");
+        
+        // Disable collision
+
+        GetNode<CollisionShape3D>("CollisionShape3D").Disabled = true;
+    }
+}
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,41 +3,132 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	// State
+	private const float SPEED = 5.0f;
+	private const float MOUSE_SENS = 0.5f;
+	
+	// References for nodes
+	private AnimatedSprite2D _animatedSprite2D;
+	private RayCast3D _rayCast3D;
+	private AudioStreamPlayer _shootSound;
+	private Control _deathScreen;
+	private Button _restartButton;
+	
+	// Player's state
+	private bool _canShoot = true;
+	private bool _dead = false;
+
+	public override void _Ready()
+	{
+		//Mouse's config.
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+		
+		// ... Nodes
+		_animatedSprite2D = GetNode<AnimatedSprite2D>("%GunAnimated");
+		_rayCast3D = GetNode<RayCast3D>("RayCast3D");
+		_shootSound = GetNode<AudioStreamPlayer>("%AudioStreamPlayer");
+		_deathScreen = GetNode<Control>("CanvasLayer/DeathScreen");
+		_restartButton = GetNode<Button>("CanvasLayer/DeathScreen/Panel/Button");
+
+		_animatedSprite2D.AnimationFinished += ShootAnimDone;
+		_restartButton.ButtonUp += Restart;
+	}
+
+	// Mouse movement
+	public override void _Input(InputEvent @event)
+	{
+		if (_dead) return;
+
+		if (@event is InputEventMouseMotion mouseMotion)
+		{
+			RotationDegrees = new Vector3(
+				RotationDegrees.X,
+				RotationDegrees.Y - mouseMotion.Relative.X * MOUSE_SENS,
+				RotationDegrees.Z
+			);
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("exit"))
+		{
+			GetTree().Quit();
+		}
+
+		if (Input.IsActionJustPressed("restart"))
+		{
+			Restart();
+		}
+		
+		if (_dead) return;
+
+		if (Input.IsActionJustPressed("shoot"))
+		{
+			Shoot();
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector3 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		//Movement
+		if (_dead) return;
+		
+		// Get the input direction and handle player movement.
+		var inputDir = Input.GetVector("move_left", "move_right", "move_forwards", "move_backwards");
+		var direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		
 		if (direction != Vector3.Zero)
 		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+			Velocity = new Vector3(
+				direction.X * SPEED,
+				Velocity.Y,
+				direction.Z * SPEED);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			Velocity = new Vector3(
+				Mathf.MoveToward(Velocity.X, 0, SPEED),
+				Velocity.Y,
+				Mathf.MoveToward(Velocity.Z, 0, SPEED));
 		}
-
-		Velocity = velocity;
+		
 		MoveAndSlide();
+	}
+
+	private void Restart()
+	{
+		GetTree().ReloadCurrentScene();
+	}
+
+	private void Shoot()
+	{
+		// Shooting mechanics.
+		if (!_canShoot) return;
+
+		_canShoot = false;
+		_animatedSprite2D.Stop();
+		_animatedSprite2D.Play("shoot");
+		_shootSound.Play();
+
+		if (!_rayCast3D.IsColliding()) return;
+		var  collider = _rayCast3D.GetCollider();
+		if (collider is Node node && node.HasMethod("Kill"))
+		{
+			node.Call("Kill");
+		}
+	}
+
+	private void ShootAnimDone()
+	{
+		_canShoot = true;
+	}
+
+	private void Kill()
+	{
+		// What happens when a player dies
+		_dead = true;
+		_deathScreen.Show();
+		Input.MouseMode = Input.MouseModeEnum.Visible;
 	}
 }
