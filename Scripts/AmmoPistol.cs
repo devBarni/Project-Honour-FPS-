5bbe28721b036b81eaa9c5df74caa1d6aecf839c
Kamil Baran <hello.barni1@gmail.com>
2024-12-04 12:47:34 +0100

    Start scripting for weapon system/ammo system

---
+++ Scripts/AmmoPistol.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class AmmoPickup : Area3D
+{
+	public override void _Ready()
+	{
+		BodyEntered += OnBodyEntered;
+	}
+
+	private void OnBodyEntered(Node3D body)
+	{
+		if (body.IsInGroup("Player"))
+		{
+			AudioController.PlayAmmoSnd(); // Ammo pickup sound
+			PlayerStats.ChangePistolAmmo(10); // Add that amount of ammo for player
+			body.Call("show_pick_overlay");
+			QueueFree();
+		}
+	}
+}
